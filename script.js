c4ee9ee5db6cbd903177ed3f678b84078e670fc2
bilal4821 <bilaldev929@gmail.com>
2024-09-02 12:53:48 +0500

    edit files

---
+++ script.js
@@ -0,0 +1,17 @@
+(function() {
+    emailjs.init({
+      publicKey: "bDUbIV4FHSIYSn4Kh",
+    });
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_yo5r7hu', 'contact_form_go2gp0b', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
